dfab2f20817481847b5ee93132eb356356b97acb
GitHub Action <action@github.com>
2021-04-01 05:48:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/fengqingxiuyi/androidstudioplugintemplate/MyBundle.kt
+++ src/main/kotlin/com/github/fengqingxiuyi/androidstudioplugintemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.fengqingxiuyi.androidstudioplugintemplate
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/fengqingxiuyi/androidstudioplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.fengqingxiuyi.androidstudioplugintemplate.services
+
+import com.github.fengqingxiuyi.androidstudioplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
